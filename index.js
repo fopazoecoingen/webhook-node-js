8e6fc077c2a84fdd2f151572c86f2adb30f2a9f6
fopazoecoingen <144165034+fopazoecoingen@users.noreply.github.com>
2024-10-22 12:08:04 -0300

    prueba

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require("express");
 
 const port = process.env.PORT || 3000;
-
+const token_api = process.env.TOKEN_API
 const app = express()
 app.use(express.json());
 
@@ -33,8 +33,43 @@ app.post("/verify",(req,res)=>{
         }
     }
     */
+
+    const axios = require('axios');
+    let data = JSON.stringify({
+    "messaging_product": "whatsapp",
+    "to": "56982677284",
+    "type": "template",
+    "template": {
+        "name": "plantilla_prueba",
+        "language": {
+        "code": "es_AR"
+        }
+    }
+    });
+
+    let config = {
+    method: 'post',
+    maxBodyLength: Infinity,
+    url: 'https://graph.facebook.com/v20.0/438734725993231/messages',
+    headers: { 
+        'Content-Type': 'application/json', 
+        'Authorization': token_api
+    },
+    data : data
+    };
+
+    axios.request(config)
+    .then((response) => {
+    console.log(JSON.stringify(response.data));
+    })
+    .catch((error) => {
+    console.log(error);
+    });
+
     res.json(req.body);
 })
+
+
 app.get("/verify",(req,res)=>{
     
   let mode = req.query["hub.mode"];
