312236266898a87d88157c095e98a86b6efa7ae9
fopazoecoingen <144165034+fopazoecoingen@users.noreply.github.com>
2024-10-23 08:45:46 -0300

    update mensaje

--- index.js
+++ index.js
@@ -33,41 +33,7 @@ app.post("/verify",(req,res)=>{
         }
     }
     */
-    const token_api = process.env.TOKEN_API;
-    console.log('Token API:', token_api);  
-    const axios = require('axios');
-    let data = JSON.stringify({
-    "messaging_product": "whatsapp",
-    "to": "56982677284",
-    "type": "template",
-    "template": {
-        "name": "plantilla_prueba",
-        "language": {
-        "code": "es_AR"
-        }
-    }
-    });
 
-    let config = {
-    method: 'post',
-    maxBodyLength: Infinity,
-    url: 'https://graph.facebook.com/v20.0/438734725993231/messages',
-    headers: { 
-        'Content-Type': 'application/json', 
-        'Authorization': token_api
-    },
-    data : data
-    };
-
-
-    console.log("Tiene que enviarme el mensaje")
-    axios.request(config)
-    .then((response) => {
-    console.log(JSON.stringify(response.data));
-    })
-    .catch((error) => {
-    console.log(error);
-    });
 
     
     res.json(req.body);
